0f2b7a0c8c3371b87f4fce69ce970710a122758c
betocostadev <robertomarx@gmail.com>
2024-10-30 16:21:02 -0300

    Feat: Changed github api tests setup to be on its own

--- src/routes/github/[user]/[repo]/github-api.spec.ts
+++ src/routes/github/[user]/[repo]/github-api.spec.ts
@@ -1,17 +1,26 @@
-import { describe, it, vi } from 'vitest'
+import { Mock, describe, it, vi, beforeEach } from 'vitest'
 import { Fetch, GithubApi } from './github-api'
 
 describe('github-api', () => {
   // Understand that this isn't a unit test, since Github can go down and the test will fail
   // We will mock it to avoid this
+  let fetchMock: Mock<Parameters<Fetch>, ReturnType<Fetch>>
+  let delayMock: Mock<[number], Promise<void>>
+  let api: GithubApi
+
+  beforeEach(() => {
+    fetchMock = vi.fn<Parameters<Fetch>, ReturnType<Fetch>>(mockPromise)
+    delayMock = vi.fn<[number], Promise<void>>(mockPromise)
+    api = new GithubApi('TOKEN', fetchMock, delayMock)
+  })
+
   describe('getRepository', () => {
     // expect is passed in the function as an argument here to avoid problems when using concurrent tests
     // there is no need to pass it when the tests are not concurrent
     it('should return repository information', async ({ expect }) => {
       // Mocking to avoid a failure due to api or communication failure
-      const fetchMock = vi.fn<Parameters<Fetch>, ReturnType<Fetch>>(mockPromise)
-      const api = new GithubApi('TOKEN', fetchMock, vi.fn(mockPromise) as any)
       const responsePromise = api.getRepository('USERNAME', 'REPOSITORY')
+
       expect(fetchMock).toHaveBeenCalledWith(
         'https://api.github.com/repos/USERNAME/REPOSITORY',
         {
@@ -30,11 +39,8 @@ describe('github-api', () => {
     it('should timeout after x seconds with timeout response', async ({
       expect,
     }) => {
-      const fetchMock = vi.fn<Parameters<Fetch>, ReturnType<Fetch>>(mockPromise)
-      const delayMock = vi.fn<[number], Promise<void>>(mockPromise)
-      const api = new GithubApi('TOKEN', fetchMock, delayMock)
-
       const responsePromise = api.getRepository('USERNAME', 'REPOSITORY')
+
       expect(fetchMock).toHaveBeenCalledWith(
         'https://api.github.com/repos/USERNAME/REPOSITORY',
         {
